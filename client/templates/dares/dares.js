3e3df35c967f85e8f6284644b97b701b5498eb3e
Aloha1981 <js945@uni-rostock.de>
2017-04-17 19:01:34 +0200

    added dare creation and display

---
+++ client/templates/dares/dares.js
@@ -0,0 +1,33 @@
+Template.dares.events({
+	'submit .open-dare-form': function (event) {
+		var name = event.target.name.value;
+		
+		var difficulty = event.target.difficulty.value;
+		var gender = event.target.gender.value;
+		var message = event.target.message.value;
+		var status = 'new';
+
+		Dares.insert({
+			 name: name,
+			 
+			 gender: gender,
+			 difficulty: difficulty,
+			 message: message,
+			 status: status,
+			 user: Meteor.userId(),
+			 createdAt: new Date()
+
+		});
+
+		$('#openDareModal').modal('hide');
+		FlashMessages.sendSuccess('Dare created')
+		return false;
+	},
+		'click .close-ticket': function () {
+			if(confirm("Are you sure?")){
+				Tickets.remove(this._id);
+				FlashMessages.sendSuccess('Dare Closed');
+				return false;
+			}
+		}
+	});
